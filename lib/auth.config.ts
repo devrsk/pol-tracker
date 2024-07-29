355a88bb89f09f41bf5f386ec0def068696b58da
Raheem SK <devrsk@tuta.io>
2024-07-29 16:42:36 -0400

    New Test Commit

--- lib/auth.config.ts
+++ lib/auth.config.ts
@@ -1,57 +1,41 @@
+// auth.config.ts
 import Credentials from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
 import { LoginSchema } from "@/lib/schema";
 import { compare } from "bcryptjs";
 import prisma from "@/lib/prisma";
 
-const authConfig: NextAuthConfig = {
+export default {
   providers: [
     Credentials({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
+      name: "credentials",
       async authorize(credentials) {
+        console.log("Received credentials:", credentials);
         const validated = LoginSchema.safeParse(credentials);
         if (validated.success) {
           const { email, password } = validated.data;
           const user = await prisma.user.findUnique({
             where: { email },
           });
-          if (user && await compare(password, user.passwordHash)) {
-            return user;
+          if (!user) {
+            console.log("User not found");
+            return null;
           }
+          const isPasswordValid = await compare(password, user.passwordHash);
+          if (!isPasswordValid) {
+            console.log("Invalid password");
+            return null;
+          }
+          console.log("User authenticated");
+          return user;
         }
+        console.log("Validation failed");
         return null;
       },
     }),
-    // Add other providers like GitHub if needed
+    // Add other providers if needed
   ],
-  callbacks: {
-    async session({ token, session }) {
-      if (token.sub && session.user) {
-        const userDB = await prisma.user.findUnique({
-          where: {
-            id: token.sub,
-          },
-          include: {
-            budgets: {
-              include: {
-                transactions: true,
-              },
-            },
-            settings: true,
-          },
-        });
-        if (userDB) {
-          session.user = userDB;
-        }
-      }
-      return session;
-    },
+  pages: {
+    error: 'error', // Custom error page URL
   },
-  session: { strategy: "jwt" },
-};
-
-export default authConfig;
+} satisfies NextAuthConfig;
