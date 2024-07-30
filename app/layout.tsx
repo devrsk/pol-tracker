8f849a4337b9cb5133e2c194876d175948451452
Raheem SK <devrsk@tuta.io>
2024-07-30 17:16:26 -0400

    New Test Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Budget Tracker",
-  description: "by Jagath",
+  title: "Def MT Bn Pol Tracker",
+  description: "by DevRSK",
 };
 
 export default function RootLayout({
